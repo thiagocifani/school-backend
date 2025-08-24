f34f20cf86789f6b0ecd35e1b128ce4e6747c967
thiagocifani <cifani.thiago@gmail.com>
2025-08-25 00:53:33 +0100

    fix: wrong method call

--- app/controllers/api/v1/cora_webhooks_controller.rb
+++ app/controllers/api/v1/cora_webhooks_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class CoraWebhooksController < BaseController
       skip_before_action :authenticate_user!, only: [:receive]
-      skip_before_action :verify_authenticity_token, only: [:receive]
+      # Note: verify_authenticity_token is not needed for API-only apps using ActionController::API
       
       # Endpoint to receive webhooks from Cora
       def receive
